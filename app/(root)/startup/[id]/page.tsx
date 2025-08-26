1e4e45d5d143582b2c7e487718c79c71ccb45709
Heshi99 <prabhashanimaheshika@gmail.com>
2025-08-26 13:38:15 +0530

    Added playlist-editor's pick

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,37 +1,53 @@
-// /startuup/id
-import { formatDate } from "@/lib/utils";
+import { Suspense } from "react";
 import { client } from "@/sanity/lib/client";
-import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
-import Link from "next/link";
+import {
+  PLAYLIST_BY_SLUG_QUERY,
+  STARTUP_BY_ID_QUERY,
+} from "@/sanity/lib/queries";
 import { notFound } from "next/navigation";
-import React, { Suspense } from "react";
+import { formatDate } from "@/lib/utils";
+import Link from "next/link";
 import Image from "next/image";
+
 import markdownit from "markdown-it";
 import { Skeleton } from "@/components/ui/skeleton";
 import View from "@/components/View";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+
+const md = markdownit();
 
 export const experimental_ppr = true;
 
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
 
-  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+  const [post, { select: editorPosts }] = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, { id }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+      slug: "editorpicks1",
+    }),
+  ]);
 
   if (!post) return notFound();
 
-  const md = new markdownit();
   const parsedContent = md.render(post?.pitch || "");
 
   return (
     <>
       <section className="pink_container !min-h-[230px]">
         <p className="tag">{formatDate(post?._createdAt)}</p>
+
         <h1 className="heading">{post.title}</h1>
         <p className="sub-heading !max-w-5xl">{post.description}</p>
       </section>
 
       <section className="section_container">
-        <img src={post.image?.asset?.url} />
+        <img
+          src={post.image}
+          alt="thumbnail"
+          className="w-full h-auto rounded-xl"
+        />
+
         <div className="space-y-5 mt-10 max-w-4xl mx-auto">
           <div className="flex-between gap-5">
             <Link
@@ -40,7 +56,7 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
             >
               <Image
                 src={post.author.image}
-                alt={post.author.name || "avatar"}
+                alt="avatar"
                 width={64}
                 height={64}
                 className="rounded-full drop-shadow-lg"
@@ -64,20 +80,28 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
               dangerouslySetInnerHTML={{ __html: parsedContent }}
             />
           ) : (
-            <p className="no-result">No Details Provided</p>
+            <p className="no-result">No details provided</p>
           )}
         </div>
 
-        <hr className="divider"/>
+        <hr className="divider" />
 
-        {/*TO DO :editor selected startups */}
+        {editorPosts?.length > 0 && (
+          <div className="max-w-4xl mx-auto">
+            <p className="text-30-semibold">Editor Picks</p>
 
-        <Suspense fallback={<Skeleton className="view_skeleton"/>}>
-            <View id={id}/>
+            <ul className="mt-7 card_grid-sm">
+              {editorPosts.map((post: StartupTypeCard, i: number) => (
+                <StartupCard key={i} post={post} />
+              ))}
+            </ul>
+          </div>
+        )}
+
+        <Suspense fallback={<Skeleton className="view_skeleton" />}>
+          <View id={id} />
         </Suspense>
       </section>
-
-      
     </>
   );
 };
